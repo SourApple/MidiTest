360303c15ed904231b90f9ea1d8f777a01c24fe1
Dalia <daliah1991@gmail.com>
2016-03-18 10:59:19 +0200

    Update parser.js

--- parser.js
+++ parser.js
@@ -232,7 +232,7 @@ function Parser(midiFile){
 
 	}
 
-	var allNotes = ['a', 'A', 'b', 'B', 'C', 'd', 'D', 'e', 'E', 'F', 'f', 'G'];
+	var allNotes = ['C', 'd', 'D', 'e', 'E', 'F', 'f', 'G', 'a', 'A', 'b', 'B'];
 	var alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 	var nextEventInfo;
